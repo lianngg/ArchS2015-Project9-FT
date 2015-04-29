6b5b328a644120c62b352c31794e2641320433c9
longma <longma@longmas-MBP.sv.cmu.local>
2015-04-29 16:41:07 -0700

    modify SensorTest.java

--- test/models/metadata/SensorCategoryTest.java
+++ test/models/metadata/SensorCategoryTest.java
@@ -2,27 +2,41 @@ package models.metadata;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SensorCategoryTest {
 
-	private static SensorCategory sensorCategory = new SensorCategory();
+	private static SensorCategory sensorCategory;
 	
 	private static String ID = "id";
 	private static String SENSOR_CATEGORY_NAME = "sensorCategoryName";
 	private static String PURPOSE = "purpose";
 	
-	public void init(){
-		sensorCategory.setId(ID);
-		sensorCategory.setSensorCategoryName(SENSOR_CATEGORY_NAME);
-		sensorCategory.setPurpose(PURPOSE);
+	
+	@Before
+	public void setUp() throws Exception{
+		sensorCategory = new SensorCategory();
 	}
 	
 	@Test
-	public void getter(){
+	public void test1(){
+		sensorCategory.setId(ID);
 		assertEquals(ID, sensorCategory.getId());
+	}
+	
+	
+	@Test
+	public void test2(){
+		sensorCategory.setSensorCategoryName(SENSOR_CATEGORY_NAME);
 		assertEquals(SENSOR_CATEGORY_NAME, sensorCategory.getSensorCategoryName());
+	}
+	
+	@Test
+	public void test3(){
+		sensorCategory.setPurpose(PURPOSE);
 		assertEquals(PURPOSE, sensorCategory.getPurpose());
 	}
+
 }
--- test/models/metadata/SensorTest.java
+++ test/models/metadata/SensorTest.java
@@ -2,15 +2,15 @@ package models.metadata;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-@RunWith(JUnit4.class)
 public class SensorTest {
 
-	private static Sensor sensor = new Sensor();
+	private static Sensor sensor;
 	
 	private static String ID = "id";
 	private static String SENSOR_NAME = "sensorName";
@@ -19,23 +19,44 @@ public class SensorTest {
 	private static String SENSOR_CATEGORY_NAME = "sensorCategoryName";
 	private static String SENSOR_USER_DEFINED_FIELDS = "sensorUserDefinedFields";
 	
-	@BeforeClass
-	public static void setter(){
-		sensor.setId(ID);
-		sensor.setSensorName(SENSOR_NAME);
-		sensor.setSensorTypeName(SENSOR_TYPE_NAME);
-		sensor.setDeviceUri(DEVICE_URI);
-		sensor.setSensorCategoryName(SENSOR_CATEGORY_NAME);
-		sensor.setSensorUserDefinedFields(SENSOR_USER_DEFINED_FIELDS);
+	@Before
+	public void setUp() throws Exception{
+		sensor = new Sensor();
 	}
 	
 	@Test
-	public void getter(){
+	public void test1(){
+		sensor.setId(ID);
 		assertEquals(ID, sensor.getId());
+	}
+	
+	@Test
+	public void test2(){
+		sensor.setSensorName(SENSOR_NAME);
 		assertEquals(SENSOR_NAME, sensor.getSensorName());
+	}
+	
+	@Test
+	public void test3(){
+		sensor.setSensorTypeName(SENSOR_TYPE_NAME);
 		assertEquals(SENSOR_TYPE_NAME, sensor.getSensorTypeName());
+	}
+	
+	@Test
+	public void test4(){
+		sensor.setDeviceUri(DEVICE_URI);
 		assertEquals(DEVICE_URI, sensor.getDeviceUri());
+	}
+	
+	@Test
+	public void test5(){
+		sensor.setSensorCategoryName(SENSOR_CATEGORY_NAME);
 		assertEquals(SENSOR_CATEGORY_NAME, sensor.getSensorCategoryName());
+	}
+	
+	@Test
+	public void test6(){
+		sensor.setSensorUserDefinedFields(SENSOR_USER_DEFINED_FIELDS);
 		assertEquals(SENSOR_USER_DEFINED_FIELDS, sensor.getSensorUserDefinedFields());
 	}
 	
